bb8fa268b33dbb2827722a81656020f3c045c0c5
Dmitry <angusmcculloch@yandex.ru>
2015-07-18 19:54:21 +0300

    Добавлен пример авторизации с помощью хеша пароля и динамичных сообщений

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -14,29 +14,36 @@ class SiteController extends Controller {
     }
 
     public function login() {
+        //Получить хеш можно с помощью Rain::app()->user->passwordHash('admin');
         $users = [
-            'admin'=>'admin'
+            'admin' => '$2y$10$0sNcjChj/QVJwcRF7GBTc.nYAlytZwKFF.EcpJ7MEqoKKHIclbtqW'
         ];
 
         if (isset($_POST['send'])) {
             $username = $_POST['username'];
             $password = $_POST['password'];
 
-            if(array_key_exists($username, $users)){
-                Rain::app()->user->login($username, $password);
-                Rain::app()->session->setMessage('success', $username. ', вы успешно вошли');
-            }
-        }
+            if (array_key_exists($username, $users)) {
+                if (Rain::app()->user->verifyPassword($password, $users[$username])) {
+                    Rain::app()->user->login($username, $password);
+                    Rain::app()->session->setMessage('success', $username . ', вы успешно вошли');
+                } else {
+                    Rain::app()->session->setMessage('error', 'Вы указали неверный пароль');
+                }
 
-        $this->render('login');
+                Rain::app()->router->refresh();
+            }
+        } else {
+            $this->render('login');
+        }
     }
 
     public function contact() {
         $this->render('contact');
     }
 
-    public function logout(){
+    public function logout() {
         Rain::app()->user->logout();
-        Rain::app()->router->redirect('site/index', ['id'=>1]);
+        Rain::app()->router->redirect('site/index', ['id' => 1]);
     }
 }
