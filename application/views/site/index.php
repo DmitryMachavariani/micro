a6c9618a67fc67cece5cc5638ae9717d9536d059
Dmitry <angusmcculloch@yandex.ru>
2015-07-19 14:20:20 +0300

    Обновление

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -3,7 +3,13 @@
 На данный момент работает файл: <span class="color"><?= __FILE__; ?></span><br/><br/>
 
 На данный момент работает контроллер: <span class="color"><?= $this->getController(); ?></span><br/>
-И вызвано действие: <span class="color"><?= $this->getAction();; ?></span>
+И вызвано действие: <span class="color"><?= $this->getAction(); ?></span>
+
+<br/>
+
+<div class="main_image">
+    <a href="http://github.com/DmitryMachavariani/micro" target="_blank"><img src="http://mentormate.com/wp-content/uploads/2014/06/GitHub.jpg" alt="image"></a>
+</div>
 
 <?php
 //$query = Rain::app()->db->select()->from('tb_users')->where(['id' => 3, 'username' => 'angus123'])->getResult();
