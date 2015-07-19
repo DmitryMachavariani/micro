c68d2b66f16bf08902dfedd492a49098730b2878
Dmitry <angusmcculloch@yandex.ru>
2015-07-19 12:35:28 +0300

    Начало разработки компонента для логгирования

--- framework/kernel/Logger.php
+++ framework/kernel/Logger.php
@@ -1,5 +1,13 @@
 <?php
 
 class Logger extends Component {
+    private $_logs = [];
 
+    public function add($text) {
+        array_push($this->_logs, $text);
+    }
+
+    public function extract() {
+        return $this->_logs;
+    }
 }
