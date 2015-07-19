ff7452389e98020b47f22ae0c30924445cd1596c
Dmitry <angusmcculloch@yandex.ru>
2015-07-19 12:36:33 +0300

    Добавлен пример использования конструктора запросов.Изменен код доступа к текущему контроллеру и действию

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -2,5 +2,11 @@
 
 На данный момент работает файл: <span class="color"><?= __FILE__; ?></span><br/><br/>
 
-На данный момент работает контроллер: <span class="color"><?= Controller::getController(); ?></span><br/>
-И вызвано действие: <span class="color"><?= Controller::getAction(); ?></span>
+На данный момент работает контроллер: <span class="color"><?= $this->getController(); ?></span><br/>
+И вызвано действие: <span class="color"><?= $this->getAction();; ?></span>
+
+
+<br/><br/><br/><br/><br/><br/><br/>
+<?php
+$query = Rain::app()->db->select()->from('tb_users')->where(['id' => 3, 'username' => 'angus123'])->getResult();
+?>
