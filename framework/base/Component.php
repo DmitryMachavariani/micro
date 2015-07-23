d4761473b9a5714c169c2d64dc0dda29290c5b3e
Dmitry <angusmcculloch@yandex.ru>
2015-07-23 20:17:36 +0300

    Добавлены методы автозагрузки классов

--- framework/base/Component.php
+++ framework/base/Component.php
@@ -1,7 +1,37 @@
 <?php
 
-class Component{
-    public function createComponent($name){
+class Component {
 
+    // Список классов для автозагрузки
+    private $_class = [
+        'BaseController' => '/base/BaseController.php',
+        'BaseValidator' => '/base/BaseValidator.php'
+    ];
+
+    // В конструкторе вызываем метод автозагрузки
+    public function __construct() {
+        $this->autoload();
+    }
+
+    public function autoload() {
+        foreach ($this->_class as $key => $value) {
+            $this->createComponent($key);
+        }
+    }
+
+    public function createComponent($name) {
+        if (isset($this->_class[$name])) {
+            $file = framework . $this->_class[$name];
+            if (is_file($file)) {
+                if (!class_exists($name)) {
+                    require_once($file);
+                    $class = new ReflectionClass($name);
+
+                    if (!$class->isAbstract()) {
+                        return new $name;
+                    }
+                }
+            }
+        }
     }
 }
