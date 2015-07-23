d0a67a033f1e3bcd681d74dc3047b9e5faec90b8
Dmitry <angusmcculloch@yandex.ru>
2015-07-24 00:41:36 +0300

    Валидатор на проверку на целочисленность

--- framework/validator/IntValidator.php
+++ framework/validator/IntValidator.php
@@ -1,5 +1,11 @@
 <?php
 
-class IntValidator extends BaseValidator{
-
+class IntValidator extends BaseValidator {
+    public static function validate($value) {
+        if (is_int($value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
