aaacb8f6c3e6d89081fea3b0ef472acd09cd73b7
Dmitry <angusmcculloch@yandex.ru>
2015-07-24 14:39:10 +0300

    Переделана авторизация с использованием хелпера FormHelper

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -2,9 +2,6 @@
 
 class SiteController extends Controller {
     public function index() {
-
-//        $result = Rain::app()->get("db")->command("SELECT * FROM tb_users")->getAll();
-//        $result = Rain::app()->get("db")->command("INSERT INTO tb_key(key_data, user_id, type)VALUES('423432432', '123121', 'activate')")->execute();
         $this->render('index');
     }
 
@@ -18,9 +15,9 @@ class SiteController extends Controller {
             'admin' => '$2y$10$0sNcjChj/QVJwcRF7GBTc.nYAlytZwKFF.EcpJ7MEqoKKHIclbtqW'
         ];
 
-        if (isset($_POST['send'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+        if ($form = FormHelper::load('rain-form-0')) {
+            $username = $form['username'];
+            $password = $form['password'];
 
             if (array_key_exists($username, $users)) {
                 if (Rain::app()->user->verifyPassword($password, $users[$username])) {
@@ -43,6 +40,6 @@ class SiteController extends Controller {
 
     public function logout() {
         Rain::app()->user->logout();
-        Rain::app()->router->redirect('site/index', ['id' => 1]);
+        Rain::app()->router->redirect('site/index');
     }
 }
