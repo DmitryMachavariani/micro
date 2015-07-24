ac155a6b5b6c0b5c0bbc193b1096324e46181e1c
Dmitry <angusmcculloch@yandex.ru>
2015-07-24 14:39:27 +0300

    Возвращена картинка

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -2,15 +2,11 @@
 
 На данный момент работает файл: <span class="color"><?= __FILE__; ?></span><br/><br/>
 
-На данный момент работает контроллер: <span class="color"><?= $this->getController(); ?></span><br/>
-И вызвано действие: <span class="color"><?= $this->getAction(); ?></span>
+На данный момент работает контроллер: <span class="color"><?= Controller::getController(); ?></span><br/>
+И вызвано действие: <span class="color"><?= Controller::getAction(); ?></span>
 
 <br/>
 
 <div class="main_image">
     <a href="http://github.com/DmitryMachavariani/micro" target="_blank"><img src="http://mentormate.com/wp-content/uploads/2014/06/GitHub.jpg" alt="image"></a>
 </div>
-
-<?php
-//$query = Rain::app()->db->select()->from('tb_users')->where(['id' => 3, 'username' => 'angus123'])->getResult();
-?>
