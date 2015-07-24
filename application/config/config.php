5bdbee00af2fa5df913a28cc426bdb99ae5e51c1
Dmitry <angusmcculloch@yandex.ru>
2015-07-24 13:17:28 +0300

    Убран роутер. Добавлены разделители для красоты

--- application/config/config.php
+++ application/config/config.php
@@ -3,23 +3,23 @@ $config = [
     'components' => [
         'session' => [
             'class' => 'Session',
+            /* --------------------- */
             'autoStart' => true
         ],
         'db' => [
             'class' => 'DataBase',
+            /* --------------------- */
             'connect' => 'mysql:host=localhost;dbname=game;',
             'username' => 'root',
             'password' => '1752dima',
             'charset' => 'utf8'
-        ],
-        'router' => [
-            'class' => 'Router'
         ],
         'user' => [
             'class' => 'User'
         ],
         'app' => [
             'class' => 'Config',
+            /* --------------------- */
             'name' => 'Моё приложение',
             'lang' => 'ru',
             'charset' => 'utf-8',
